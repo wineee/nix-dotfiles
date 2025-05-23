b8999473167b2e81b117320ae0d2552382abf82f
rewine <luhongxu@deepin.org>
2025-05-23 11:10:50 +0800

    Update

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # 使用稳定版修改为 "github:nixos/nixpkgs/nixos-25.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
